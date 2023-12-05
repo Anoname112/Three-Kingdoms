e5039420a0b88cec06bb58ff76bdca059a21a344
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2023-12-05 11:05:19 +0000

    Implement disband unit

--- snippets.js
+++ snippets.js
@@ -1,13 +1,3 @@
-// Disband a unit
-function disbandUnit (unitId) {
-    for (var i = 0; i < units.length; i++) {
-        if (units[i].Id == unitId) {
-            units.splice(i, 1);
-            break;
-        }
-    }
-}
-
 // Distribute food evenly to each force's cities. This also works for cities[i].Gold
 var total = 0;
 var force = officers[getOfficerIndexByName('Cao Cao')].Force;
