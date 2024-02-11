834c54c8f3ee0b719a66d5aac1a8d96955c81a12
Anoname112 <44809772+Anoname112@users.noreply.github.com>
2024-02-11 16:38:43 +0700

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -60,9 +60,9 @@ playerForce = officers[player].Force;
 openPlayerCard();
 
 // Add Jiang Wei to Cao Cao Forces, then position him in Chen Liu
-officers[204].Force = 0;
-officers[204].City = 8;
-officers[204].Position = new Point(24, 12);
+officers[getOfficerIndexByName('Jiang Wei')].Force = 0;
+officers[getOfficerIndexByName('Jiang Wei')].City = 8;
+officers[getOfficerIndexByName('Jiang Wei')].Position = new Point(24, 12);
 
 // Construct famous officers array, with battle stats and ability (delete is optional)
 var famous = ['Cao Cao', 'Xiahou Yuan', 'Liu Bei', 'Guan Yu', 'Sun Ce', 'Zhou Yu', 'Lu Bu', 'Zhang Liao', 'Gongsun Zan', 'Zhao Yun', 'Ma Teng', 'Ma Chao'];
